1e89f21568b7522a8b24f0febee941531abfae85
LenAnderson <anderson.len.ja@gmail.com>
2025-01-14 17:55:04 -0400

    remove unused imports

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import { Generate, callPopup, chat, eventSource, event_types, messageFormatting, saveChatConditional, saveSettingsDebounced, substituteParams } from '../../../../script.js';
+import { Generate, chat, eventSource, event_types, messageFormatting, saveChatConditional, saveSettingsDebounced, substituteParams } from '../../../../script.js';
 import { extension_settings } from '../../../extensions.js';
 import { SlashCommand } from '../../../slash-commands/SlashCommand.js';
-import { ARGUMENT_TYPE, SlashCommandArgument, SlashCommandNamedArgument } from '../../../slash-commands/SlashCommandArgument.js';
 import { SlashCommandParser } from '../../../slash-commands/SlashCommandParser.js';
-import { delay, isTrueBoolean } from '../../../utils.js';
+import { delay } from '../../../utils.js';
 
 
 
